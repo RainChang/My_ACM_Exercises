bc86139b940fc0afbebf5ac556474efe813496ec
Rain <changfanglei@gmail.com>
2017-03-20 16:48:47 +0800

    指针测试

---
+++ huawei/point_test/point_test/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  point_test
+//
+//  Created by Rain on 18/03/2017.
+//  Copyright © 2017 Rain. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    int* pint = 0;
+    pint += 6;
+    cout<< pint <<endl;
+    return 0;
+}
